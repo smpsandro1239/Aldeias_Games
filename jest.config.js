8fd45422715efe6ecb80e6bc6baaa4a200e216ed
Sandro Pereira <smpsandro1239@gmail.com>
2026-04-15 10:49:43 +0100

    feat(jest): corrigir configuração Jest para ambiente ESM
    - Mantida configuração em CommonJS para compatibilidade com Jest
    - Ajustado ts-jest para uso correto de ESM
    - Estabilizado ambiente de testes

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,23 @@
 /** @type {import('jest').Config} */
 const config = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/mocks/fileMock.js',
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
+
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-  collectCoverageFrom: [
-    "src/lib/**/*.ts",
-    "!src/lib/**/*.d.ts",
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
+
+  transformIgnorePatterns: ['node_modules/(?!jose/)'],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
 };
 
 module.exports = config;
