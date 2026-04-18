9867b658463c60dd0b099207a561ca101db1ecda
Sandro Pereira <smpsandro1239@gmail.com>
2026-04-18 17:24:01 +0100

    fix: remove checkbox import

--- src/components/modals/create-evento-modal.tsx
+++ src/components/modals/create-evento-modal.tsx
@@ -15,7 +15,6 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Switch } from "@/components/ui/switch";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Gift, Star, Award, Gamepad2 } from "lucide-react";
 
 interface EventoData {
